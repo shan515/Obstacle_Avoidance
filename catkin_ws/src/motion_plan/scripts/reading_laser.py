dbaa9f6ea56f7542c4bf512ed8048af7027f5ceb
shan515 <shantanupande898@gmail.com>
2020-05-16 13:10:16 +0530

    Motion planning added

---
+++ catkin_ws/src/motion_plan/scripts/reading_laser.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import LaserScan
+
+def callback_laser(msg):
+  # 120 degrees into 3 regions
+  # receive a value of range between 0 and 10.
+  regions = [ 
+    min(min(msg.ranges[0:2]), 10),
+    min(min(msg.ranges[3:5]), 10),
+    min( min(msg.ranges[6:9]), 10),
+    ]
+
+  rospy.loginfo(regions)
+
+def main():
+  rospy.init_node('reading_laser')
+  sub= rospy.Subscriber("/m2wr/laser/scan", LaserScan, callback_laser)
+
+  rospy.spin()
+
+if __name__ == '__main__':
+  main()
